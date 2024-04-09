6a049ab1d22603e1d49fbc094c23c53ed0d702d5
neo-kjb <115832171+neo-kjb@users.noreply.github.com>
2024-04-10 02:03:47 +0300

    Displaying courses

--- educa/courses/views.py
+++ educa/courses/views.py
@@ -1,5 +1,5 @@
 from django.views.generic.list import ListView
-from .models import Course, Module, Content
+from .models import Course, Module, Content, Subject
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.urls import reverse_lazy
@@ -9,6 +9,8 @@ from .forms import ModuleFormSet
 from django.forms.models import modelform_factory
 from django.apps import apps
 from braces.views import CsrfExemptMixin, JsonRequestResponseMixin
+from django.db.models import Count
+from django.views.generic.detail import DetailView
 
 
 class OwnerMixin:
@@ -153,3 +155,23 @@ class ContentOrderView(CsrfExemptMixin, JsonRequestResponseMixin, View):
                 order=order
             )
         return self.render_json_response({"saved": "OK"})
+
+
+class CourseListView(TemplateResponseMixin, View):
+    model = Course
+    template_name = "courses/course/list.html"
+
+    def get(self, request, subject=None):
+        subjects = Subject.objects.annotate(total_courses=Count("courses"))
+        courses = Course.objects.annotate(total_modules=Count("modules"))
+        if subject:
+            subject = get_object_or_404(Subject, slug=subject)
+            courses = courses.filter(subject=subject)
+        return self.render_to_response(
+            {"subjects": subjects, "subject": subject, "courses": courses}
+        )
+
+
+class CourseDetailView(DetailView):
+    model = Course
+    template_name = "courses/course/detail.html"
