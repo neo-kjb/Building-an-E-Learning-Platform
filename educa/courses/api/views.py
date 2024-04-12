e209b316a7908581842490e8f520639d38d2cf84
neo-kjb <115832171+neo-kjb@users.noreply.github.com>
2024-04-12 18:54:02 +0300

    Serializing course contents

--- educa/courses/api/views.py
+++ educa/courses/api/views.py
@@ -1,6 +1,10 @@
 from rest_framework import generics
 from courses.models import Subject, Course
-from courses.api.serializers import SubjectSerializer, CourseSerializer
+from courses.api.serializers import (
+    SubjectSerializer,
+    CourseSerializer,
+    CourseWithContentsSerializer,
+)
 from django.shortcuts import get_object_or_404
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -8,6 +12,7 @@ from rest_framework.authentication import BasicAuthentication
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import viewsets
 from rest_framework.decorators import action
+from courses.api.permissions import IsEnrolled
 
 
 class SubjectListView(generics.ListAPIView):
@@ -44,3 +49,13 @@ class CourseViewSet(viewsets.ReadOnlyModelViewSet):
         course = self.get_object()
         course.students.add(request.user)
         return Response({"enrolled": True})
+
+    @action(
+        detail=True,
+        methods=["get"],
+        serializer_class=CourseWithContentsSerializer,
+        authentication_classes=[BasicAuthentication],
+        permission_classes=[IsAuthenticated, IsEnrolled],
+    )
+    def contents(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
