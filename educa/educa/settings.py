62dbb9488800876a0782bcf74ba406dfbed82931
neo-kjb <115832171+neo-kjb@users.noreply.github.com>
2024-04-13 18:10:01 +0300

    Enabling a channel layer

--- educa/educa/settings.py
+++ educa/educa/settings.py
@@ -167,3 +167,12 @@ REST_FRAMEWORK = {
 
 
 ASGI_APPLICATION = "educa.asgi.application"
+
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [("127.0.0.1", 6379)],
+        },
+    },
+}
